c95895c2f87e8f10a1db662a118e60286ef156bf
Atomdevnp <mukeshdevnp@gmail.com>
2016-07-31 23:50:53 +0545

    unity mvc bootstrap by ms added

---
+++ eCommerceDemo/eCommerce.Contracts/Repositories/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace eCommerce.Contracts.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Commit();
+        void Delete(object id);
+        void Dispose();
+        IQueryable<TEntity> GetAll(object id);
+        TEntity GetById(object id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
--- eCommerceDemo/eCommerce.WebUI/Controllers/HomeController.cs
+++ eCommerceDemo/eCommerce.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using eCommerce.DAL.Data;
+﻿using eCommerce.Contracts.Repositories;
+using eCommerce.DAL.Data;
 using eCommerce.DAL.Repositories;
+using eCommerce.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,18 @@ namespace eCommerce.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepositoryBase<Customer> customers;
+
+        public HomeController(IRepositoryBase<Customer> customers) {
+            this.customers = customers;
+        }
         public ActionResult Index()
         {
-            CustomerRepository customer = new CustomerRepository(new DataContext());
-            ProductRepositories product = new ProductRepositories(new DataContext());
+            //CustomerRepository customer = new CustomerRepository(new DataContext());
+            //ProductRepositories product = new ProductRepositories(new DataContext());
+
+            //customers = new CustomerRepository(new DataContext());
+
             return View();
         }
 
